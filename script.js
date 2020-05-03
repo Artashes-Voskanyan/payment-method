54b7f3c553f0b50de1b8cb5999945f14fb7ff151
Artashes-Voskanyan <62055368+Artashes-Voskanyan@users.noreply.github.com>
2020-05-03 21:25:18 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function FlipCard() {
+  document.getElementById("container").style.display = "block";
+}
+function FlipCash() {
+  document.getElementById("container1").style.display = "block";
+}
+
+function buttonFunc() {
+  var x=document.getElementById("button1").value;
+  document.getElementById("but1").innerHTML;
+}
